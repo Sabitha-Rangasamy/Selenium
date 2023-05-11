bdffcf5ffebce2c1a65d0b73bdcdb40fef5f5bba
Sabitha-Rangasamy <rsabi2726@gmail.com>
2023-05-10 22:30:54 -0400

    Day1
    Datatype, Class, Package

--- src/avinashclass/Program1.java
+++ src/avinashclass/Program1.java
@@ -5,6 +5,8 @@ public class Program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("The month of May was come, when every lust heart beginneth to blossom, and to bring forth fruit");
+
 	}
 
 }
